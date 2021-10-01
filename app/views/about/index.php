dcd5c9d941641291bac56f2e7467b9f7cf05f969
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2021-10-01 14:48:55 +0700

    Membuat tampilan dengan bootstrap, serta menambahkan navbar dan foto agar membuat tampilan menjadi menarik

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,6 @@
-
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-5">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/luffy.jpg" alt="Hikmat Fadrial R" width="250" height="250" class="rounded-circle shadow">
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang 
     <?= $data['pekerjaan']; ?>.</p>
+    </div>
